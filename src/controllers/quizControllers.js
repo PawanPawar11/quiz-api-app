670ce1b27c75987d94fefe3a20267360ab648d73
PawanPawar11 <pawanpawar4369@gmail.com>
2025-09-27 23:08:46 +0530

    feat: Implement quiz taking and scoring logic

--- src/controllers/quizControllers.js
+++ src/controllers/quizControllers.js
@@ -35,3 +35,72 @@ export const createQuizQuestions = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+export const getAllQuizTitle = async (req, res) => {
+  try {
+    const quizzes = await Quiz.find().select("title");
+    res.json(quizzes);
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
+  }
+};
+
+export const getQuizQuestions = async (req, res) => {
+  try {
+    const { quizId } = req.params;
+    const questions = await Question.find({ quizId }).select(
+      "-correctOptionId"
+    );
+
+    res.json(questions);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const submitAnswerAndGetScore = async (req, res) => {
+  try {
+    const { quizId } = req.params;
+    const { answers } = req.body;
+
+    if (!Array.isArray(answers)) {
+      return res.status(400).json({ error: "Answers must be in array format" });
+    }
+
+    const totalQuestions = await Question.countDocuments({ quizId });
+
+    if (totalQuestions === 0) {
+      return res
+        .status(400)
+        .json({ error: "No questions are found for this quiz" });
+    }
+
+    const questionIds = answers.map((a) => a.questionId);
+
+    const correctAnswers = await Question.find({
+      _id: questionIds,
+      quizId: quizId,
+    }).select("correctOptionId");
+
+    let score = 0;
+
+    for (const userAnswer of answers) {
+      const { questionId, selectedOptionId } = userAnswer;
+
+      const correctAnswerDoc = correctAnswers.find(
+        (q) => q._id.toString() === questionId
+      );
+
+      if (
+        correctAnswerDoc &&
+        correctAnswerDoc.correctOptionId === selectedOptionId
+      ) {
+        score++;
+      }
+    }
+
+    res.json({ score, total: totalQuestions });
+  } catch (error) {
+    res.status(500).json({ error: "Server error", details: error.message });
+  }
+};
--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   createQuizQuestions,
   createQuizTitle,
+  getAllQuizTitle,
+  getQuizQuestions,
+  submitAnswerAndGetScore,
 } from "../controllers/quizControllers.js";
 
 const router = express.Router();
@@ -9,5 +12,11 @@ const router = express.Router();
 router.post("/", createQuizTitle);
 
 router.post("/:quizId/questions", createQuizQuestions);
+
+router.get("/", getAllQuizTitle);
+
+router.get("/:quizId/questions", getQuizQuestions);
+
+router.post("/:quizId/submit", submitAnswerAndGetScore);
 
 export default router;
